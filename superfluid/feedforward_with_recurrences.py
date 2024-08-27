9db6563839e0123c0342d025af5bf88d3a153e4a
anhinga <anhinga@users.noreply.github.com>
2024-08-27 11:57:16 -0400

    Create feedforward_with_recurrences.py

---
+++ superfluid/feedforward_with_recurrences.py
@@ -0,0 +1,7 @@
+# trying to sketch a feedword network with fluid neurons and optional local recurrences
+
+# fluid neurons will have linear combinations of these activation functions
+
+soft_activations = ['accum_add_args', 'max_norm_dict', 'dot_product', 'compare_scalars', 'const_1', 'const_end']
+
+
