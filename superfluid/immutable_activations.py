a79ef8aecbba27768d834f1c0a8a12c55fd7b995
anhinga <michael.bukatin@gmail.com>
2024-09-02 11:09:08 -0400

    making this more JAX-friendly (not quite yet)

--- superfluid/immutable_activations.py
+++ superfluid/immutable_activations.py
@@ -1,4 +1,3 @@
-
 def matrix_element(to_neuron, to_input, from_neuron, from_output, value = 1.0):
     return {to_neuron: {to_input: {from_neuron: {from_output: value}}}}
 
@@ -53,11 +52,22 @@ def const_end(all_inputs):
 def timer_add_one(all_inputs):
     return {'timer': {':number': getn(getv(all_inputs, 'timer')) + 1.0}}
 
+import jax.lax
+
 def input_dummy(all_inputs):
+    def true_branch(_):
+        return {'char': {s[i:i+1]: 1.0}}
+
+    def false_branch(_):
+        return {'char': {}}
+        
     t = relu(getn(getv(all_inputs, 'timer')))
     s = "test string."
-    i = min(round(t // 10), len(s) - 1)
-    return {'char': {s[i:i+1]: 1.0} if t % 10 == 0 else {}}
+    i = jnp.minimum(round(t // 10), len(s) - 1)
+ 
+    return jax.lax.cond(t % 10 == 0, true_branch, false_branch, operand=None) 
+    # the following is re-written in a JAX-friendly way
+    # return {'char': {s[i:i+1]: 1.0} if t % 10 == 0 else {}}
 
 def output_dummy(all_inputs):
     return {'dict-1': getv(all_inputs, 'dict-1'), 'dict-2': getv(all_inputs, 'dict-2')}
