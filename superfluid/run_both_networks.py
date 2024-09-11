195ba974ad7b48b93a9a812150890f2514db1922
anhinga <michael.bukatin@gmail.com>
2024-09-11 18:25:40 -0400

    this is what we run for full exploration

--- superfluid/run_both_networks.py
+++ superfluid/run_both_networks.py
@@ -60,8 +60,8 @@ print(time.time()-start_time, " seconds")
 """
 
 def loss_fn(changing_output):
-    trace, _ = reduce(one_iteration, range(2), ({}, {'input': {}, 'output': changing_output}))
-    trace_manual, _ = reduce(one_iteration, range(2), ({}, {'input': {}, 'output': initial_output_manual}))
+    trace, _ = reduce(one_iteration, range(140), ({}, {'input': {}, 'output': changing_output}))
+    trace_manual, _ = reduce(one_iteration, range(140), ({}, {'input': {}, 'output': initial_output_manual}))
     first = [trace[key]['input']['output']['dict-1'][':number'] for key in trace if key != 0]
     second = [trace[key]['input']['output']['dict-2'][':number'] for key in trace if key != 0]
     first_manual = [trace_manual[key]['input']['output']['dict-1'][':number'] for key in trace_manual if key != 0]
